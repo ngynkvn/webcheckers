03f0b7cbe41a1cf6c24321dfcdf7830727ffcda0
Kev <ngynkvn@gmail.com>
2017-10-21 16:21:03 -0500

    Room creation

--- client/src/components/Lobby.js
+++ client/src/components/Lobby.js
@@ -1,25 +1,48 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import path from 'path'
 
-export default class Lobby extends Component { 
+export default class Lobby extends Component {
     constructor(props) {
         super(props);
         this.state = {};
     }
-    
-    componentDidMount() { 
-        let ws = new WebSocket();
-    }
 
-    render() { 
-        return (<div>
-        <p>Hello, {this.props.user}!</p>
-        <p>Create a new lobby:</p>    
-        <form onSubmit={(e) => (this.submit(), e.preventDefault())}>
-        <label htmlFor="roomName" >Room Name: </label>
-        <input type="text" name="roomname" onChange={(e) => this.handleChange(e)}/>
-        <input type="submit" value="Submit"/>
-        </form>  
-        </div>    
-        )    
+    componentDidMount() {}
+
+    handleChange(val) {
+        this.setState({roomName: val});
+    }
+    makeRoom(roomID) {
+        fetch(path.join('/create_room/', this.props.user, roomID)).then((res) => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                this.setState({success: false});
+            }
+        }).then(info => {
+            this.setState({success: true, game: info});
+        });
+    }
+    render() {
+        return (
+            <div>
+                <p>Hello, {this.props.user}!</p>
+                <p>Create a new lobby:</p>
+                <form
+                    onSubmit={(e) => {
+                    this.makeRoom(this.state.roomName);
+                    e.preventDefault();
+                }}>
+                    <label htmlFor="roomName">Room Name:
+                    </label>
+                    <input
+                        type="text"
+                        name="n"
+                        autoFocus={true}
+                        onChange={(e) => this.handleChange(e.target.value)}/>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
+        )
     }
 }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,26 +1,41 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Login extends Component {
-    constructor(props) { 
+    constructor(props) {
         super(props);
-        this.state = { name: "" };
-        
+        this.state = {
+            name: ""
+        };
+
     }
     handleChange(e) {
         let val = e.target.value;
-        this.setState({ name: val });
+        this.setState({name: val});
         // console.log(this.state.name);
     }
     submit() {
         console.log(this.state.name);
-        this.props.sendData(this.state.name);
-     }
-    render() { 
-        return (<form onSubmit={(e) => (this.submit(), e.preventDefault())}>
-            <label htmlFor="username" >name : </label>
-            <input type="text" name="username" onChange={(e) => this.handleChange(e)}/>
-            <input type="submit" value="Submit"/>
-        </form>);
+        this
+            .props
+            .sendData(this.state.name);
+    }
+    render() {
+        return (
+            <form
+                onSubmit={(e) => {
+                this.submit();
+                e.preventDefault()
+            }}>
+                <label htmlFor="username">name :
+                </label>
+                <input
+                    type="text"
+                    autoFocus={true}
+                    name="username"
+                    onChange={(e) => this.handleChange(e)}/>
+                <input type="submit" value="Submit"/>
+            </form>
+        );
     }
 }
 
